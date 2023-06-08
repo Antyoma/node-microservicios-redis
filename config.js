3d4953ea83087176d66e749827fc1a70eae49373
Anthony Matheus <antyoma33@gmail.com>
2023-06-08 15:54:28 -0500

    Intentando conectar con una bd Mysql

--- config.js
+++ config.js
@@ -5,4 +5,11 @@ module.exports = {
     jwt: {
         secret: process.env.JWT_SECRET || 'notasecret!',
     },
+    mysql: {
+        host: process.env.MYSQL_HOST || 'database-1.cadsdujhcxxt.us-east-1.rds.amazonaws.com',
+        user: process.env.MYSQL_USER || 'admin',
+        password: process.env.MYSQL_PASS || 'User_2023$',
+        database: process.env.MYSQL_DB || 'database-1',
+        port: process.env.MYSQL_PORT || 3360,
+    },
 }
